9afa67e55737d562be291f75ed393351a2ef4881
Jonathan Lepolt <lepolt@gmail.com>
2025-01-27 11:12:23 -0500

    Initial Commit

---
+++ Swift Data Test/Swift_Data_TestApp.swift
@@ -0,0 +1,32 @@
+//
+//  Swift_Data_TestApp.swift
+//  Swift Data Test
+//
+//  Created by Jonathan Lepolt on 1/27/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Swift_Data_TestApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
